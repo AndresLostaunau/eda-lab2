882a53b4c6fb34edc994ef59cb31381d0ad2c342
Andres Lostaunau <andres.lostaunau@utec.edu.pe>
2021-09-30 14:24:43 -0500

    node structure v0.1

--- spatial/RangeTree1D.hpp
+++ spatial/RangeTree1D.hpp
@@ -5,14 +5,71 @@
 namespace utec {
 namespace spatial {
 
+  /**
+   * RT1DNode
+   */
+
+  template<typename Point>
+  class RT1DNode:public SpatialBase<Point>{
+    private:
+      Point *content;
+      RT1DNode *left;
+      RT1DNode *right;
+    public:
+      RT1DNode(){};
+      RT1DNode(Point *c):content(c),left(nullptr),right(nullptr){};
+
+      void setContent(Point _content){
+        *content = _content;
+      }
+
+      Point *getContent(){
+        return content;
+      }
+
+      void insert(Point _content){
+        if(content > _content){
+          if(left == nullptr){
+            left = new RT1DNode(_content);
+          }else{
+            left->insert(_content);
+          }
+        }else{
+          if(right == nullptr){
+            right = new RT1DNode(_content);
+          }else{
+            right->insert(_content);
+          }
+        }
+      }
+
+      void leftRotation(){
+        Point aux;
+        aux = *content;
+        *content = *(left->content);
+        *(left->content) = aux;
+      }
+
+      void rightRotation(){
+        Point aux;
+        aux = *content;
+        *content = *(right->content);
+        *(right->content) = aux;
+      }
+  };
+
 /**
  * RangeTree1D implementation
  */
 template <typename Point>
 class RangeTree1D : public SpatialBase<Point> {
  private:
+  RT1DNode *root;
  public:
-  RangeTree1D(){};
+  RangeTree1D(){
+    root = new RT1DNode();
+  };
+
   void insert(const Point& new_point) override {}
 
   // El punto de referencia no necesariamente es parte del dataset
