ccfb3d93e71607a836009c8f0d758ef4f4b1a68d
Andres Lostaunau <andres.lostaunau@utec.edu.pe>
2021-09-30 21:50:48 -0500

    debug rtree

--- spatial/RangeTree1D.hpp
+++ spatial/RangeTree1D.hpp
@@ -5,20 +5,19 @@
 namespace utec {
 namespace spatial {
 
-  /**
-   * RT1DNode
-   */
-
-  template<typename Point>
-  class RT1DNode:public SpatialBase<Point>{
-    private:
+/**
+ * RangeTree1D implementation
+ */
+template <typename Point>
+class RangeTree1D : public SpatialBase<Point> {
+   private:
       Point content;
       int height;
-      RT1DNode *left;
-      RT1DNode *right;
+      RangeTree1D<Point> *left;
+      RangeTree1D<Point> *right;
     public:
-      RT1DNode(){};
-      RT1DNode(Point c):content(c),height(1),left(nullptr),right(nullptr){};
+      RangeTree1D<Point>(){};
+      RangeTree1D<Point>(Point c):content(c),height(1),left(nullptr),right(nullptr){};
 
       void setContent(Point _content){
         content = _content;
@@ -28,11 +27,11 @@ namespace spatial {
         height = h;
       }
 
-      void setLeft(RT1DNode *l){
+      void setLeft(RangeTree1D<Point> *l){
         left = l;
       }
 
-      void setRight(RT1DNode *r){
+      void setRight(RangeTree1D<Point> *r){
         right = r;
       }
 
@@ -44,19 +43,19 @@ namespace spatial {
         return height;
       }
 
-      RT1DNode *getLeft(){
+      RangeTree1D<Point> *getLeft(){
         return left;
       }
 
-      RT1DNode *getRight(){
+      RangeTree1D<Point> *getRight(){
         return right;
       }
 
       void leftRotation(){
         Point aux;
-        RT1DNode *aux_ptr;
+        RangeTree1D<Point> *aux_ptr;
         aux = this->getContent();
-        this->setContent = this->getRight()->getContent();
+        this->setContent(this->getRight()->getContent());
         this->getRight()->setContent(aux);
 
         aux_ptr = this->getLeft();
@@ -68,7 +67,7 @@ namespace spatial {
 
       void rightRotation(){
         Point aux;
-        RT1DNode *aux_ptr;
+        RangeTree1D<Point> *aux_ptr;
         aux = this->getContent();
         this->setContent(this->getLeft()->getContent());
         this->getLeft()->setContent(aux);
@@ -117,61 +116,62 @@ namespace spatial {
         }
       }
 
-      int insert(Point _content){
-        int l=1, r=1, aux;
-        if(this->content > _content){
-          if(this->left == nullptr){
-            aux = this->getContent();
-            this->setContent(_content);
-            this->setLeft(new RT1DNode(_content));
-            this->setRight(new RT1DNode(aux));
-            this->setHeight(2);
-            return 2;
-          }else{
-            l = this->getLeft()->insert(_content);
-          }
-        }else{
+      void insert(const Point& _content) override {
+        int l=1, r=1;
+        Point aux;
+        if(this->content < _content){
           if(this->right == nullptr){
             aux = this->getContent();
             this->setContent(_content);
-            this->setLeft(new RT1DNode(_content));
-            this->setRight(new RT1DNode(aux));
+            this->setLeft(new RangeTree1D<Point>(_content));
+            this->setRight(new RangeTree1D<Point>(aux));
             this->setHeight(2);
-            return 2;
           }else{
-            r = this->getRight()->insert(_content);
+            this->getRight()->insert(_content);
+          }
+        }else{
+            if(this->left == nullptr){
+            aux = this->getContent();
+            this->setContent(_content);
+            this->setLeft(new RangeTree1D<Point>(_content));
+            this->setRight(new RangeTree1D<Point>(aux));
+            this->setHeight(2);
+          }else{
+            this->getLeft()->insert(_content);
           }
         }
         if(l > r){
-          this->setHeight(l+1);
+          this->setHeight(this->getLeft()->getHeight()+1);
         }else{
-          this->setHeight(r+1);
+          this->setHeight(this->getRight()->getHeight()+1);
         }
-        balance()
-        return this->height;
+        balance();
       }
-  };
-
-/**
- * RangeTree1D implementation
- */
-template <typename Point>
-class RangeTree1D : public SpatialBase<Point> {
- private:
-  RT1DNode *root;
- public:
-  RangeTree1D(){
-    root = new RT1DNode();
-  };
-
-  void insert(const Point& new_point) override {
-    root->insert(new_point);
-  }
 
   // El punto de referencia no necesariamente es parte del dataset
   Point nearest_neighbor(const Point& reference) override { return Point({0}); }
+
+  void rec_range(std::vector<Point> *ans_vec, RangeTree1D<Point> *it, const Point& min, const Point& max){
+    if(it->getLeft()==nullptr){
+      if((min < it->getContent() or it->getContent()==min) && (it->getContent() < max or it->getContent()==max) ){
+        ans_vec->push_back(it->getContent());
+      }
+    }
+
+    if(it->getContent() < min){
+      rec_range(ans_vec,it->getRight(),min,max);
+    }else if(max < it->getContent()){
+      rec_range(ans_vec,it->getLeft(),min,max);
+    }else{
+      rec_range(ans_vec,it->getLeft(),min,max);
+      rec_range(ans_vec,it->getRight(),min,max);
+    }
+  }
+
   std::vector<Point> range(const Point& min, const Point& max) override {
-    return {};
+    std::vector<Point> ans_vec;
+    rec_range(&ans_vec,this,min,max);
+    return ans_vec;
   };
 };
 
